58353dfe4f163eed750c16b182e63b422ce2ac53
Bruno Oliveira <bruno.lima@confia.net.br>
2022-11-20 19:38:08 -0300

    entrada de dados em c

--- C/inputData.c
+++ C/inputData.c
@@ -2,11 +2,15 @@
 
 int main()
 {
-	int num;
+	int num, soma;
 	
 	printf("\nDigite um número: ");
 	scanf("%d", &num);
 
+	soma = num + 5;
+
 	printf("\nO valor da variável 'num' é: %d\n", num);
+	printf("\nO valor da variável 'num' + 5 é: %d\n", soma);
+	
 	return 0;
 }
