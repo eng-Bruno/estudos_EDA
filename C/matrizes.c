20368fa6de23a9f3b2c4f9c539ac50f7d47f2775
Bruno Oliveira <bruno.lima@confia.net.br>
2022-12-11 19:54:42 -0300

    matriz em c

---
+++ C/matrizes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int mat[2][3];
+
+int main() {
+
+	for(int i=0; i<2; i++){
+		for(int j=0; j<3; j++){
+			printf("Digite um número: ");
+			scanf("%d", &mat[i][j]);
+		}
+	}
+
+	for(int i=0; i<2; i++){
+		for(int j=0; j<3; j++){
+			printf("O valor de mat[%d][%d] = %d\n", i, j, mat[i][j]);
+		}
+	}
+	
+  return 0;
+}
