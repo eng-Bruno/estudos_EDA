d9020576eb4a4cf6e3f535190611e276b86b4432
Bruno Oliveira <bruno.lima@confia.net.br>
2023-02-08 19:31:50 -0300

    Estrutura de dados: filas

---
+++ Filas e Pilhas/filas.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Pessoa
+{
+	int senha;
+	struct pessoa *prox;
+};
+
+typedef struct pessoa Pessoa;
+
+Pessoa *fila;
+
+void add(int senha){ // adicionar ao final da fila
+	
+	Pessoa *p = (Pessoa*) malloc(sizeof(Pessoa));
+	p->senha = senha;
+	p->prox = NULL;
+	
+	if(fila==NULL){
+		fila = p;
+	} else{
+		Pessoa *filaAux = fila;
+		while(filaAux->prox!=NULL){
+			filaAux = filaAux->prox;
+		};
+		filaAux->prox = p;
+	}
+}
+
+void remover(){
+	if(fila==NULL){
+		printf("Fila vazia");
+	}else{
+		fila = fila->prox;
+	}
+}
+
+void imprime(Pessoa *f){
+	if(f!=NULL){
+		printf("\nSenha da Pessoa: %d", f->senha);
+		imprime(f->prox);
+	}
+}
+
+int main(void) {
+  add(5);
+	add(8);
+	add(10);
+	add(22);
+
+	Pessoa *filaAux = fila;
+	imprime(filaAux);
+
+	printf("\n");
+	
+	remover();
+
+	filaAux = fila;
+	imprime(filaAux);
+
+	printf("\n");
+	
+	remover();
+	
+	filaAux = fila;
+	imprime(filaAux);
+  return 0;
+}
